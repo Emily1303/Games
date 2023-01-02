6e1fec4959fae5217751cd8705b8d04425ac4664
Emily1303 <e.filipova1303@gmail.com>
2023-01-02 13:11:56 +0200

    Added "Snake"

---
+++ Snake/com/company/SnakeGame.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import com.company.GameFrame;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+
+        GameFrame frame = new GameFrame();
+    }
+}
